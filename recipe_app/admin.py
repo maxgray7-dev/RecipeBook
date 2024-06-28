1f278e4c7dbe0df4c019031c4a4ed405b6e48346
Maksim Popov <exactlyy@gmail.com>
2024-06-28 13:00:58 +0000

    editing settings to allow running server

--- recipe_app/admin.py
+++ recipe_app/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe
 from . import models
 
 
-# Register your models here.
-admin.site.register(models.Recipe)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
+    summernote_fields = ('content',)
+
+
+
+
+
+#Register your models here
--- recipe_book/urls.py
+++ recipe_book/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipe_app.urls')),
+    path('summernote/', include('django_summernote.urls')),
+
 ] 
