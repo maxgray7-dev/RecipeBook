d55f41908ae2c553f0c03b2e3a8d4280018f97b0
Maksim Popov <exactlyy@gmail.com>
2024-06-29 20:32:55 +0000

    overwriting css

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -19,18 +19,14 @@ class RecipeList(generic.ListView):
     # The code below will displays only objects that have status "published"
     queryset = Recipe.objects.all()
 
-    # template_name = 'recipe_app/recipe_list.html'
     template_name = 'recipe_app/index.html'
     paginate_by = 9
 
 
-# def recipes_detail(request, title):
-#     queryset = Recipe.objects.filter(status=1)
-#     post=get_object_or_404(queryset, title=title)
-
-
-
-#     return render(
-#         request, recipe_app/'recipes_detail.html',
+def recipes_detail(request, title):
+    queryset = Recipe.objects.filter(status=1)
+    post=get_object_or_404(queryset, slug=title)
+    return render(
+        request, recipe_app/'recipes_detail.html',
   
-# )
+)
