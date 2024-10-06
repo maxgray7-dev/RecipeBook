1eb982c8e4cc9d220222cb5a8fbd1088096e75b0
Maksim Popov <exactlyy@gmail.com>
2024-10-06 17:35:40 +0000

    Fixed the issue with adding recipe

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://udgoxghaxi9:FJlyHFRe3Eax@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/rack_donor_sage_74067")
+os.environ.setdefault(
+    "SECRET_KEY", "SnxbHsqRazLHjVXLOrLWSj68jE9fS2aG")
+os.environ.setdefault(
+    "CLOUDINARY_URL",  "cloudinary://641858126431978:ZwX9A8qK8d-3z6opov2pyR-fvZM@do5ytloen")
