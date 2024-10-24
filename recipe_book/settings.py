0f7d8ff06c73b1800e9e53e973006d6a22c9f09f
Maksim Popov <exactlyy@gmail.com>
2024-10-24 16:18:26 +0000

    saving

--- recipe_book/settings.py
+++ recipe_book/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-maxgray7dev-recipebook-m9a3i0oyc47.ws-eu116.gitpod.io',
+ALLOWED_HOSTS = ['8000-maxgray7dev-recipebook-m168v9fn8q0.ws-eu116.gitpod.io',
 '.herokuapp.com',
 '.codeinstitute-ide.net',
 ]
