231631df753cab088110634d417f69dbed17ef18
Maksim Popov <exactlyy@gmail.com>
2025-01-04 16:30:50 +0000

    updated database , migration link

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://udgoxghaxi9:FJlyHFRe3Eax@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/rack_donor_sage_74067")
+    "DATABASE_URL", "postgresql://neondb_owner:LKFYjQ4n6Tst@ep-twilight-brook-a2w3twkv.eu-central-1.aws.neon.tech/rack_donor_sage_74067")
 os.environ.setdefault(
     "SECRET_KEY", "SnxbHsqRazLHjVXLOrLWSj68jE9fS2aG")
 os.environ.setdefault(
